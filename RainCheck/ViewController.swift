90fe286cbb6187357449ab89bfb632122e8a5884
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-07-09 13:44:46 +0300

    Added WeatherTableViewCell configuring with .xib and API-responce logic

--- RainCheck/ViewController.swift
+++ RainCheck/ViewController.swift
@@ -8,21 +8,124 @@
 import UIKit
 import CoreLocation
 
-struct Weather {
+struct WeatherResponse: Codable {
+    let latitude: Float
+    let longitude: Float
+    let timezone: String
+    let currently: CurrentWeather
+    let hourly: HourlyWeather
+    let daily: DailyWeather
+    let offset: Float
+}
 
+struct CurrentWeather: Codable {
+    let time: Int
+    let summary: String
+    let icon: String
+    let nearestStormDistance: Int
+    let nearestStormBearing: Int
+    let precipIntensity: Int
+    let precipProbability: Int
+    let temperature: Double
+    let apparentTemperature: Double
+    let dewPoint: Double
+    let humidity: Double
+    let pressure: Double
+    let windSpeed: Double
+    let windGust: Double
+    let windBearing: Int
+    let cloudCover: Double
+    let uvIndex: Int
+    let visibility: Double
+    let ozone: Double
+}
+
+struct DailyWeather: Codable {
+    let summary: String
+    let icon: String
+    let data: [DailyWeatherEntry]
+}
+
+struct DailyWeatherEntry: Codable {
+    let time: Int
+    let summary: String
+    let icon: String
+    let sunriseTime: Int
+    let sunsetTime: Int
+    let moonPhase: Double
+    let precipIntensity: Float
+    let precipIntensityMax: Float
+    let precipIntensityMaxTime: Int
+    let precipProbability: Double
+    let precipType: String?
+    let temperatureHigh: Double
+    let temperatureHighTime: Int
+    let temperatureLow: Double
+    let temperatureLowTime: Int
+    let apparentTemperatureHigh: Double
+    let apparentTemperatureHighTime: Int
+    let apparentTemperatureLow: Double
+    let apparentTemperatureLowTime: Int
+    let dewPoint: Double
+    let humidity: Double
+    let pressure: Double
+    let windSpeed: Double
+    let windGust: Double
+    let windGustTime: Int
+    let windBearing: Int
+    let cloudCover: Double
+    let uvIndex: Int
+    let uvIndexTime: Int
+    let visibility: Double
+    let ozone: Double
+    let temperatureMin: Double
+    let temperatureMinTime: Int
+    let temperatureMax: Double
+    let temperatureMaxTime: Int
+    let apparentTemperatureMin: Double
+    let apparentTemperatureMinTime: Int
+    let apparentTemperatureMax: Double
+    let apparentTemperatureMaxTime: Int
+}
+
+struct HourlyWeather: Codable {
+    let summary: String
+    let icon: String
+    let data: [HourlyWeatherEntry]
+}
+
+struct HourlyWeatherEntry: Codable {
+    let time: Int
+    let summary: String
+    let icon: String
+    let precipIntensity: Float
+    let precipProbability: Double
+    let precipType: String?
+    let temperature: Double
+    let apparentTemperature: Double
+    let dewPoint: Double
+    let humidity: Double
+    let pressure: Double
+    let windSpeed: Double
+    let windGust: Double
+    let windBearing: Int
+    let cloudCover: Double
+    let uvIndex: Int
+    let visibility: Double
+    let ozone: Double
 }
 
 class ViewController: UIViewController {
 
   //MARK: - Properties
-  var models = [Weather]()
+  var models = [DailyWeatherEntry]()
   let locationManager = CLLocationManager()
   var currentLocation: CLLocation?
 
   //MARK: - Outlets
   @IBOutlet private weak var table: UITableView!
 
-//MARK: - View Lifecycle
+  //MARK: - View Lifecycle
   override func viewDidLoad() {
     super.viewDidLoad()
     table.register(HourlyTableViewCell.nib(), forCellReuseIdentifier: HourlyTableViewCell.identifier)
@@ -41,8 +144,26 @@ class ViewController: UIViewController {
     guard let currentLocation = currentLocation else { return }
     let longitude = currentLocation.coordinate.longitude
     let latitude = currentLocation.coordinate.latitude
-    let url = "https://api.darksky.net/forecast/ddcc4ebb2a7c9930b90d9e59bda0ba7a/37.8267,-122.4233?exclude=[flags,minutely]"
     print("longitude: \(longitude)| latitude: \(latitude)")
+    let url = "https://api.darksky.net/forecast/ddcc4ebb2a7c9930b90d9e59bda0ba7a/\(latitude),\(longitude)?exclude=[flags,minutely]"
+    URLSession.shared.dataTask(with: URL(string: url)!) { data, response, error in
+      guard let data = data, error == nil else {
+        print("Failed to get data witout error's - \(String(describing: error))")
+        return
+      }
+      var json: WeatherResponse?
+      do {
+        json = try JSONDecoder().decode(WeatherResponse.self, from: data)
+      } catch {
+        print("Decoding error - \(error)")
+      }
+      guard let result = json else { return }
+      let entries = result.daily.data
+      self.models.append(contentsOf: entries)
+      DispatchQueue.main.async {
+        self.table.reloadData()
+      }
+    }.resume()
   }
 }
 
@@ -61,8 +182,6 @@ extension ViewController: CLLocationManagerDelegate {
       requestWeatherForLocation()
     }
   }
-
-
 }
 
 //MARK: - TableView Delegate & DataSource
@@ -72,7 +191,9 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
   }
 
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    return UITableViewCell()
+    let cell = tableView.dequeueReusableCell(withIdentifier: WeatherTableViewCell.identifier, for: indexPath) as! WeatherTableViewCell
+    cell.configure(with: models[indexPath.row])
+    return cell
   }
 
 
--- RainCheck/WeatherCell/WeatherTableViewCell.swift
+++ RainCheck/WeatherCell/WeatherTableViewCell.swift
@@ -11,19 +11,36 @@ class WeatherTableViewCell: UITableViewCell {
 
   static let identifier = "WeatherTableViewCell"
 
+  @IBOutlet private weak var dayLabel: UILabel!
+  @IBOutlet private weak var highTemperatureLabel: UILabel!
+  @IBOutlet private weak var lowTemperatureLabel: UILabel!
+  @IBOutlet private weak var iconImageView: UIImageView!
+
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+      backgroundColor = .gray
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
     }
 
   static func nib() -> UINib {
     return UINib(nibName: "WeatherTableViewCell", bundle: nil)
+  }
+
+  func configure(with model: DailyWeatherEntry) {
+    self.lowTemperatureLabel.text = "\(Int(model.temperatureLow))°"
+    self.lowTemperatureLabel.text = "\(Int(model.temperatureHigh))°"
+    self.dayLabel.text = getDayForDate(Date(timeIntervalSince1970: Double(model.time)))
+    self.iconImageView.image = UIImage(named: "clear")
+  }
+
+  func getDayForDate(_ date: Date?) -> String {
+    guard let inputDate = date else { return ""}
+    let formatter = DateFormatter()
+    formatter.dateFormat = "ddd"
+    return formatter.string(from: inputDate)
   }
     
 }
